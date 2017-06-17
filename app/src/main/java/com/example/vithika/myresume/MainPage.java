b58ec7f4c11ef27264bb737f1dc79a2f1a240c4a
MasterShrey19 <jain.shrey2419@gmail.com>
2017-06-17 16:42:18 +0530

    Animation in main page and intent to second

--- app/src/main/java/com/example/vithika/myresume/MainPage.java
+++ app/src/main/java/com/example/vithika/myresume/MainPage.java
@@ -1,13 +1,50 @@
 package com.example.vithika.myresume;
 
+import android.content.Intent;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class MainPage extends AppCompatActivity {
 
-    @Override
+    public void welcome(View view)
+    {
+        Button signUp=(Button)findViewById(R.id.btnSignUp);
+        signUp.animate()
+                .translationYBy(-150f)
+                .scaleX(0f)
+                .scaleY(0f)
+                .setDuration(2000);
+
+        Button signIn=(Button)findViewById(R.id.btnSignIn);
+        signIn.animate()
+                .translationYBy(150f)
+                .scaleX(0f)
+                .scaleY(0f)
+                .setDuration(2000);
+
+    }
+
+    public void func_SignUp(View view)
+    {
+        //Button signUp=(Button)findViewById(R.id.btnSignUp);
+        welcome(view);
+        //end(view);
+        Intent up=new Intent(this,CreateAccount.class);
+        startActivity(up);
+    }
+
+    public void func_SignIn(View view)
+    {
+        welcome(view);
+        Intent in=new Intent(this,LoginPage.class);
+        startActivity(in);
+    }
+        @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main_page);
+
     }
 }
